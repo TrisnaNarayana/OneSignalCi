08865db1b233587482d37e378cad78e15e1d5e52
TrisnaNarayana <trisnanarayana@gmail.com>
2018-08-20 16:14:49 +0700

    blabla

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+define('CMS_URI', 'http://localhost/onesignal');
+
+// ID_KEY onesignal.com
+define('APPID', 'e849ee6f-500d-482b-b922-c9ba96d3f01c');
+define('APKEY', 'MzVhNzFiM2QtNDk5ZC00MDFkLWI5ZGYtY2QwZmM2MDFjODFi');
+// Start massege
+define('ACTIONMESSAGE' , 'We\'d like to show you notifications for the latest news.');
+define('ACCEPTBUTTONTEXT', 'ALLOW');
+define('CANCELBUTTONTEXT', 'NO THANKS');
+
+
+define('URLDEFOLTIMG', 'http://ci3onesignal.codefort.ru/push/img/9397-ya.timkas-priroda-azar-013.gif');
+
+
+?>
